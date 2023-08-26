78f2fb4454ce106bf8030f7621b35daa4849229a
natroshviliio <ioane.natroshvili08b@geolab.edu.ge>
2023-08-26 10:23:02 +0400

    calculators

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { Col } from 'react-bootstrap';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import "../node_modules/bpg-arial-caps/css/bpg-arial-caps.min.css";
+import CalculateFromXLSX from './Components/CalculateFromXLSX';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={
+        <Col className='d-flex gap-3 align-items-center p-4'>
+          <Link to='/uniques'>UNIQUES</Link>
+        </Col>
+      } />
+      <Route path='/uniques' element={<CalculateFromXLSX />} />
+    </Routes>
   );
 }
 
---
+++ src/Components/CalculateFromXLSX.js
@@ -0,0 +1,207 @@
+import React, { useEffect, useState } from 'react'
+import { Button, Col, Form } from 'react-bootstrap'
+import { IoAdd } from "react-icons/io5";
+import { BsTrash3Fill } from "react-icons/bs";
+
+const CalculateFromXLSX = () => {
+    const [leftvalues, setLeftValues] = useState([{ value: 0 }]);
+    const [rightValues, setRightValues] = useState([{ value: 0 }]);
+    const [leftFiltered, setLeftFiltered] = useState([]);
+    const [rightFiltered, setRightFiltered] = useState([]);
+    const [leftJoinedValues, setLeftJoinedValues] = useState([]);
+    const [rightJoinedValues, setRightJoinedValues] = useState([]);
+
+    const addLeftValue = () => {
+        const _leftValues = [...leftvalues];
+        _leftValues.push({ value: '' });
+        setLeftValues(_leftValues);
+    }
+    const removeLeftValue = (i) => {
+        let _leftValues = [...leftvalues];
+        _leftValues = _leftValues.filter((_, idx) => idx !== i);
+        setLeftValues(_leftValues);
+    }
+    const changeLeftValue = (e, i) => {
+        const _leftValues = [...leftvalues];
+        _leftValues[i].value = e.target.value;
+        setLeftValues(_leftValues);
+    }
+
+
+    const addRightValues = () => {
+        const _rightValues = [...rightValues];
+        _rightValues.push({ value: '' });
+        setRightValues(_rightValues);
+    }
+    const removeRightValues = (i) => {
+        let _rightValues = [...rightValues];
+        _rightValues = _rightValues.filter((_, idx) => idx !== i);
+        setRightValues(_rightValues);
+    }
+    const changeRightValue = (e, i) => {
+        const _rightValues = [...rightValues];
+        _rightValues[i].value = e.target.value;
+        setRightValues(_rightValues);
+    }
+
+    const getUniques = (variant) => {
+        let _leftValues = leftvalues.map(x => Number(x.value));
+        let _rightValues = rightValues.map(x => Number(x.value));
+        const _leftJoinedValues = [];
+        const _rightJoinedValues = [];
+
+
+        const rightUnique = (left, right = []) => {
+            for (let k = 0, u = 0; ;) {
+                let _right = [...right];
+                const sum = right.slice(k, k + u + 1).reduce((a, b) => a + b, 0);
+                const indexes = right.slice(k, k + u + 1).map(x => right.indexOf(x));
+                _right.splice(k, k + u + 1);
+
+                for (let j = 0; j < _right.length; j++) {
+                    const _sum = sum + _right[j];
+                    if (left === _sum) {
+                        const _joinedValues = [...right.slice(k, k + u + 1), _right[j]];
+                        const index13 = _leftValues.indexOf(_leftValues.find(x => x === left));
+                        _rightJoinedValues.push([_joinedValues.join(' + '), `${left} - left`, left]);
+                        right = right.filter((_, idx) => !indexes.includes(idx));
+                        const index12 = right.indexOf(_right[j]);
+                        right = right.filter((_, idx) => idx !== index12);
+                        _leftValues = _leftValues.filter((_, idx) => idx !== index13);
+                        _rightValues = right;
+                        return 0;
+                    }
+                }
+                u >= right.length ? (() => { k++; u = 0 })() : u++;
+                if (k >= right.length - 1 && u >= right.length - 1) break;
+            }
+            _rightValues = right;
+        };
+
+        const leftUnique = (right, left = []) => {
+            let sum;
+            let _left = [];
+            let indexes = [];
+            for (let k = 0, u = 0; ;) {
+                _left = [...left];
+                sum = left.slice(k, k + u + 1).reduce((a, b) => a + b, 0);
+                indexes = left.slice(k, k + u + 1).map(x => left.indexOf(x))
+                _left.splice(k, k + u + 1);
+
+                for (let j = 0; j < _left.length; j++) {
+                    const _sum = sum + _left[j];
+                    if (right === _sum) {
+                        const _joinedValues = [...left.slice(k, k + u + 1), _left[j]];
+                        const index13 = _rightValues.indexOf(_rightValues.find(x => x === right));
+                        _leftJoinedValues.push([_joinedValues.join(' + '), `${right} - right`, right]);
+                        left = left.filter((_, idx) => !indexes.includes(idx));
+                        const index12 = left.indexOf(_left[j]);
+                        left = left.filter((_, idx) => idx !== index12);
+                        _rightValues = _rightValues.filter((_, idx) => idx !== index13);
+                        _leftValues = left;
+                        return 0
+                    }
+                }
+                u >= left.length ? (() => { k++; u = 0 })() : u++;
+                if (k >= left.length - 1 && u >= left.length - 1) break;
+            }
+            _leftValues = left;
+        };
+
+
+        if (variant === 1) {
+            _leftValues.map((x, i) => {
+                rightUnique(x, _rightValues, []);
+            });
+
+            _rightValues.map(x => {
+                leftUnique(x, _leftValues, []);
+            });
+        } else {
+            _rightValues.map(x => {
+                leftUnique(x, _leftValues, []);
+            });
+
+            _leftValues.map((x, i) => {
+                rightUnique(x, _rightValues, []);
+            });
+        }
+
+        setRightJoinedValues([..._rightJoinedValues]);
+        setRightFiltered(_rightValues);
+
+        setLeftJoinedValues([..._leftJoinedValues]);
+        setLeftFiltered(_leftValues);
+
+
+
+        // console.log(_rightJoinedValues);
+    }
+
+    // useEffect(() => {
+    //     console.log(leftJoinedValues);
+    //     console.log(rightJoinedValues);
+    // }, [leftJoinedValues, rightJoinedValues])
+
+    return (
+        <Col className='bpg-arial-caps'>
+            <Col className='fs-3 d-flex align-items-center border border-0 border-bottom border-secondary p-3' style={{ fontWeight: "bold" }}>
+                UNIQUE NUMBERS + SUM
+            </Col>
+            <Col className='col-10 mx-auto d-flex gap-3 p-2'>
+                <Col className='p-3 d-flex flex-column gap-2 col-3'>
+                    {leftvalues.map((x, i) => {
+                        return (
+                            <Col key={i} className='col-12 d-flex gap-2'>
+                                <Form.Control type='number' className='shadow-sm rounded-1' size='lg' value={x.value} placeholder={`left value ${i}`} onChange={(e) => changeLeftValue(e, i)} />
+                                {i > 0 && <Button variant='danger' className='px-3' onClick={() => removeLeftValue(i)}><BsTrash3Fill /></Button>}
+                            </Col>
+                        )
+                    })}
+                    <Button className='col-12' onClick={addLeftValue}><IoAdd className='fs-4' style={{ fontWeight: "bold" }} /></Button>
+                </Col>
+                <Col className='p-3 d-flex flex-column gap-2 col-3'>
+                    {rightValues.map((x, i) => {
+                        return (
+                            <Col key={i} className='col-12 d-flex gap-2'>
+                                <Form.Control type='number' className='shadow-sm rounded-1' size='lg' value={x.value} placeholder={`right value ${i}`} onChange={(e) => changeRightValue(e, i)} />
+                                {i > 0 && <Button variant='danger' className='px-3' onClick={() => removeRightValues(i)}><BsTrash3Fill /></Button>}
+                            </Col>
+                        )
+                    })}
+                    <Button className='col-12' onClick={addRightValues}><IoAdd className='fs-4' style={{ fontWeight: "bold" }} /></Button>
+                </Col>
+                <Col className='p-3'>
+                    <Col className='d-flex align-items-center gap-3'>
+                        <Button variant='warning' onClick={() => getUniques(1)}>Get Uniques Variant 1</Button>
+                        <Button variant='warning' onClick={() => getUniques(2)}>Get Uniques Variant 2</Button>
+                    </Col>
+                    <Col className='mt-3'>
+                        <Col className='d-flex align-items-center gap-2'><span style={{ fontWeight: "bold" }}>LEFT UNIQUES:</span>{leftFiltered?.join(', ')}</Col>
+                        <Col className='d-flex align-items-center gap-2'><span style={{ fontWeight: "bold" }}>INDEXES:</span>
+                            {leftJoinedValues.map((x, i) => {
+                                return (
+                                    <Col key={i} className='col-auto p-2 border border-1 shadow-sm'>{x[2]} = {x[0]}</Col>
+                                )
+                            })}
+                        </Col>
+                        <Col className='d-flex align-items-center gap-2'><span style={{ fontWeight: "bold" }}>SUM:</span>{leftFiltered?.reduce((a, b) => a + b, 0)}</Col>
+                    </Col>
+                    <Col className='mt-3'>
+                        <Col className='d-flex align-items-center gap-2'><span style={{ fontWeight: "bold" }}>RIGHT UNIQUES:</span>{rightFiltered?.join(', ')}</Col>
+                        <Col className='d-flex align-items-center gap-2'><span style={{ fontWeight: "bold" }}>INDEXES:</span>
+                            {rightJoinedValues.map((x, i) => {
+                                return (
+                                    <Col key={i} className='col-auto p-2 border border-1 shadow-sm'>{x[2]} = {x[0]}</Col>
+                                )
+                            })}
+                        </Col>
+                        <Col className='d-flex align-items-center gap-2'><span style={{ fontWeight: "bold" }}>SUM:</span>{rightFiltered?.reduce((a, b) => a + b, 0)}</Col>
+                    </Col>
+                </Col>
+            </Col>
+        </Col>
+    )
+}
+
+export default CalculateFromXLSX
